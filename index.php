3e6c1e00a0e6f340d748fdbd6b84e6325c5a08b3
IceCream <maeldupuy49@gmail.com>
2023-12-25 10:33:09 +0100

    images profil réglé

--- index.php
+++ index.php
@@ -318,7 +318,7 @@ $db->close();
                                     class="flex items-center justify-between w-full py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
                                     <img id="avatarButton" type="button" data-dropdown-toggle="userDropdown"
                                         data-dropdown-placement="bottom-start"
-                                        class="w-10 h-10 rounded-full cursor-pointer" src="./ppclients/basic.png"
+                                        class="w-10 h-10 rounded-full cursor-pointer" src="./images/ppclients/basic.png"
                                         alt="User dropdown">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                         stroke-width="2" d="m1 1 4 4 4-4" />
